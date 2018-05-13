331878e83db9fd0a6a4163fd30831aa71066dd46
fedor <fedor.malyshkin@yandex.ru>
2018-05-13 18:23:04 +0300

    initial commit

---
+++ src/main/scala/hackerrank_fp/FP_01_UpdateList.scala
@@ -0,0 +1,5 @@
+package hackerrank_fp
+
+object FP_01_UpdateList {
+	def f(arr:List[Int]):List[Int] = {List.empty}
+}
---
+++ src/test/scala/hackerrank_fp/FP_01_UpdateListTest.scala
@@ -0,0 +1,27 @@
+package bitworks.test_case
+
+import hackerrank_fp.FP_01_UpdateList
+import org.scalatest.FlatSpec
+
+class FP_01_UpdateListTest extends FlatSpec {
+
+	behavior of "FP_01_UpdateList"
+
+	it should "updateList" in {
+		val in = List.apply(2, -4, 3, -1, 23, -4, -54)
+		val out = FP_01_UpdateList.f(in)
+
+		assert(out.equals(List.apply(
+			2,
+			4,
+			3,
+			1,
+			23,
+			4,
+			54))
+		)
+
+	}
+
+
+}
