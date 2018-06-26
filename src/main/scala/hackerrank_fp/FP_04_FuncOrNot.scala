993d301a8053d55209d17fe3cdbac8555146b8bf
fedor <fedor.malyshkin@yandex.ru>
2018-06-26 17:32:26 +0300

    +1

---
+++ src/main/scala/hackerrank_fp/FP_04_FuncOrNot.scala
@@ -0,0 +1,18 @@
+package hackerrank_fp
+
+/**
+  * https://www.hackerrank.com/challenges/functions-or-not/problem
+  */
+object FP_04_FuncOrNot {
+
+    def main(args: Array[String]) {
+        val n = io.StdIn.readInt()
+        (1 to n).foreach { i =>
+            val k = io.StdIn.readInt()
+            val list = (1 to k).map(x => io.StdIn.readLine.split("\\s+")(0)).toList
+            if (list.distinct.size == list.size) println("YES")
+            else println("NO")
+        }
+    }
+
+}
