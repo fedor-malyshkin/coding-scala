0b378eafd0e21cbe64052320335e6e44440a0ae1
fedor.malyshkin <fedor.malyshkin@yandex.ru>
2021-04-13 00:03:20 +0400

    Module 5 - solve tasks

--- src/test/scala/udemy/advanced1/module5/Module05.scala
+++ src/test/scala/udemy/advanced1/module5/Module05.scala
@@ -4,8 +4,12 @@
 package udemy.advanced1.module5
 
 import org.scalatest.SeveredStackTraces
+import org.scalatest.exceptions.TestFailedException
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+
+import scala.reflect.ClassTag
+import scala.util.control.NonFatal
 
 class Module05 extends AnyFunSuite with Matchers with SeveredStackTraces {
 
@@ -23,39 +27,57 @@ class Module05 extends AnyFunSuite with Matchers with SeveredStackTraces {
   // now, define an object ReversableString as an implementation of Reversable for String,
   // that simply calls reverse on the string.
   // Make it implicit
+  implicit object ReversableString extends Reversable[String] {
+    override def reverse(x: String): String = x.reverse
+  }
 
   // uncomment the following test to make sure it works
-
-  /*
   test("ReversableString reverses a given string") {
-    reverse("hello") should be ("olleh")
-    reverse("") should be ("")
+    reverse("hello") should be("olleh")
+    reverse("") should be("")
   }
-  */
 
   // next define an object ReversableInt which does the same thing for a Reversable[Int], by reversing the digits
   // but still returning an Int, e.g. 1458 becomes 8541
-
-  /*
-  test("ReversableInt reverses the digits in an Int") {
-    reverse(12345) should be (54321)
-    reverse(1) should be (1)
-    reverse(100) should be (1) // this is why this is a bit of a crap example :-)
+  implicit object ReversableInt extends Reversable[Int] {
+    override def reverse(x: Int): Int = Integer.parseInt(x.toString.reverse)
   }
-  */
+  test("ReversableInt reverses the digits in an Int") {
+    reverse(12345) should be(54321)
+    reverse(1) should be(1)
+    reverse(100) should be(1) // this is why this is a bit of a crap example :-)
+  }
+
 
   // Write an implicit def to compose any T with reversable into a reverser for a List of T
   // that reverses both the contents of the List, and the List itself. Uncomment below to test.
+  implicit def reversableList[R: Reversable]: Reversable[List[R]] = new Reversable[List[R]] {
+    val reverser: Reversable[R] = implicitly[Reversable[R]]
 
-  /*
+    override def reverse(x: List[R]): List[R] = x
+      .map(reverser.reverse)
+      .reverse
+  }
+
+
   test("Reverse a List of Ints") {
-    reverse(List(123, 456, 100)) should be (List(1, 654, 321))
+    reverse(List(123, 456, 100)) should be(List(1, 654, 321))
   }
 
   test("Reverse a List of Strings") {
-    reverse(List("hello", "world")) should be (List("dlrow", "olleh"))
+    reverse(List("hello", "world")) should be(List("dlrow", "olleh"))
   }
-  */
+
+  def interceptException[E: ClassTag](a: => Unit): Unit = {
+    val ct = implicitly[ClassTag[E]]
+    try {
+      a
+    } catch {
+      case _: E => return
+      case r => fail(s"Blimey! Wrong exception. Expected ${ct.runtimeClass} was ${r.getClass}")
+    }
+    fail("Blimey! No exception at all.")
+  }
 
   test("Create your own intercept method") {
     // using an implicit class tag, create a new interceptException[T] method such that the following tests pass.
@@ -66,7 +88,6 @@ class Module05 extends AnyFunSuite with Matchers with SeveredStackTraces {
     // that the exception was not thrown. If another exception than the one expected is thrown, fail with a
     // suitable method as well.
 
-    /*
     // intercept a division by zero error
     interceptException[ArithmeticException] {
       val x = 1 / 0
@@ -83,12 +104,11 @@ class Module05 extends AnyFunSuite with Matchers with SeveredStackTraces {
     // and finally test that our interceptException detects the wrong kind of exception
     interceptException[TestFailedException] {
       interceptException[NumberFormatException] {
-        val x = 1 / 0   // this is an ArithmeticException, not a NumberFormatException
+        val x = 1 / 0 // this is an ArithmeticException, not a NumberFormatException
       }
     }
-    */
-  }
 
+  }
 
   test("Implicit parameter example") {
 
@@ -109,21 +129,44 @@ class Module05 extends AnyFunSuite with Matchers with SeveredStackTraces {
 
       case class Interval(value: Int)
 
-      def eventually(f: => Unit) {
-        val maxTries = MaxTries(10)
-        val interval = Interval(100)
+      implicit val maxTries: MaxTries = MaxTries(10)
+      implicit val interval: Interval = Interval(100)
+
+
+      def eventually(f: => Unit)(implicit maxTries: MaxTries, interval: Interval) {
         eventuallyWith(maxTries.value, interval.value)(f)
       }
 
-      def eventuallyWith(maxTries: Int, interval: Int)(f: => Unit): Unit = ???
+      def eventuallyWith(maxTries: Int, interval: Int)(f: => Unit): Unit = {
+        try {
+          f
+        } catch {
+          case NonFatal(e) => if (maxTries > 0) {
+            Thread.sleep(interval)
+            eventuallyWith(maxTries - 1, interval)(f)
+          } else
+            throw e
+        }
+      }
     }
+
 
     import Eventually._
 
     val list = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
     val it = list.iterator
+
     eventually {
       it.next should be(6)
     }
+
+    val it2 = list.iterator
+    interceptException[TestFailedException] {
+      eventually {
+        it2.next should be(77)
+      }
+    }
   }
+
+
 }
