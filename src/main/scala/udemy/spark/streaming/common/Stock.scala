8a5666068d83435c5f4fe7295c04671f05dac883
fedor.malyshkin <fedor.malyshkin@gmail.com>
2021-07-19 01:32:47 +0400

    Remove spark code + dependencies (are moved to coding-spark repo)

--- src/main/scala/udemy/spark/streaming/common/Stock.scala
+++
@@ -1,9 +0,0 @@
-package udemy.spark.streaming.common
-
-import java.sql.Date
-
-case class Stock(
-                  company: String,
-                  date: Date,
-                  value: Double
-                )
